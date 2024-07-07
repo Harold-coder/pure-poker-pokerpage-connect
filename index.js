227ee0f328824d4c527392c3b9e62b6642ecc3db
Harold-coder <hjc2154@columbia.edu>
2024-07-07 21:02:37 +0200

    Middleware implemented.

--- index.js
+++ index.js
@@ -9,11 +9,54 @@ exports.handler = async (event) => {
     const gameId = event.queryStringParameters ? event.queryStringParameters.gameId : null;
     const playerId = event.queryStringParameters ? event.queryStringParameters.playerId : null;
 
+    // Note: playerId = jwtToken
+    if (!playerId) {
+        return { statusCode: 500, body: JSON.stringify({ message: 'Failed to connect, no jwt given.', action: 'connect' }) };
+    }
+
+    let userId = undefined;
+    try {
+        const res = await fetch(
+          //error 500
+          "https://oqqznkdgb3.execute-api.us-east-1.amazonaws.com/dev/validate_token",
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${playerId}`,
+            },
+          }
+        );
+        console.log("Response:", res);
+        if (res.status === 200) {
+          const data = await res.json();
+          userId = data.user.username;
+        } else {
+          return {
+            statusCode: 500,
+            body: JSON.stringify({
+              message: "Failed to validate token",
+              action: "poker-connect",
+            }),
+            headers: headerTemplate,
+          };
+        }
+      } catch (err) {
+        console.error("Error validating token:", err);
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            message: "Failed to validate token",
+            action: "poker-connect",
+          }),
+          headers: headerTemplate,
+        };
+      }
+
     const item = {
         connectionId: connectionId,
         // Only add gameId and playerId to the item if they are provided
         ...(gameId && { gameId: gameId }),
-        ...(playerId && { playerId: playerId }),
+        ...(userId && { playerId: userId }),
     };
 
     try {
